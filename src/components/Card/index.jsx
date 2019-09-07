5438050d1048a28960dafa3f0a53c9903241e952
Harry Prabowo <harry.prabowo@tokopedia.com>
2019-09-07 15:45:54 +0700

    init structure

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Card = () => {
+    return (
+        <Fragment>
+            Hello world!
+        </Fragment>
+    )
+}
+
+export default Card
---
+++ src/containers/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+const Dashboard = () => {
+    return (
+        <Fragment>
+            Hello world!
+        </Fragment>
+    )
+}
+
+export default Dashboard
