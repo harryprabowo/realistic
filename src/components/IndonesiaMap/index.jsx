8cf152a8120e4881329907fabac818a6a5783235
Asif Hummam Rais <hashshura@gmail.com>
2019-09-10 02:19:28 +0700

    Implement react-simple-map for Indonesia component

---
+++ src/components/IndonesiaMap/index.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from 'react'
+
+import {
+    ComposableMap,
+    ZoomableGroup,
+    Geographies,
+    Geography,
+    Markers,
+    Marker,
+} from "react-simple-maps" 
+
+import ReactTooltip from "react-tooltip"
+
+import TopoJson from './topo.json'
+
+import './style.scss'
+
+const IndonesiaMap = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            ReactTooltip.rebuild()
+        }, 100)
+    })
+
+    return (
+        <div className="map-container">
+            <ComposableMap
+                projection={'mercator'}
+                projectionConfig={{
+                    scale: 1000,
+                }}
+            >
+                <ZoomableGroup
+                    center={[ 118, -3 ]}
+                >
+                    <Geographies geography={TopoJson} >
+                        {(geographies, projection) =>
+                            geographies.map((geography, i) =>
+                                <Geography
+                                    key={i}
+                                    data-tip={geography.properties.NAME_1}
+                                    geography={geography}
+                                    projection={projection}
+                                    style={{
+                                        default: {
+                                            fill: "#ECEFF1",
+                                            stroke: "#607D8B",
+                                            strokeWidth: 0.75,
+                                            outline: "none",
+                                        },
+                                        hover: {
+                                            fill: "#CFD8DC",
+                                            stroke: "#607D8B",
+                                            strokeWidth: 1,
+                                            outline: "none",
+                                        },
+                                        pressed: {
+                                            fill: "#FF5722",
+                                            stroke: "#607D8B",
+                                            strokeWidth: 1,
+                                            outline: "none",
+                                        }
+                                    }}
+                                >
+                                    {console.log(geography)}
+                                </Geography>
+                            )
+                        }
+                    </Geographies>
+                </ ZoomableGroup>
+            </ ComposableMap>
+            <ReactTooltip/>
+        </div>
+    )
+}
+
+export default IndonesiaMap
--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 
-import { Card } from '../../components'
+import {
+    Row,
+    Col
+} from 'react-bootstrap'
+
+import {
+    Card,
+    IndonesiaMap
+} from '../../components'
 
 import './style.scss'
-
 
 const Dashboard = () => {
     return (
         <div id="Dashboard">
             <div className="filter-card">
-                <Card.Container>
-                    <Card.Title>A Card Title</Card.Title>
-                    <Card.Content>This is a card.</Card.Content>
-                </Card.Container>
+                <Row>
+                    <Col sm={3}/>
+                    <Col sm={{span: 6}}>
+                        <Row>
+                            <Col sm={1}/>
+                            <Col sm={{span: 10}}>
+                                <Card.Container>
+                                    <Card.Title>A Card Title</Card.Title>
+                                    <Card.Content>This is a card.</Card.Content>
+                                </Card.Container>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
             </div>
+
+            <IndonesiaMap/>
         </div>
     )
 }
